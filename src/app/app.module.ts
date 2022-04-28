536a7c96f9ae9f195668bb95221a61c589b58c84
VINICIUS LIMA DE SANTANA <vinicius.lsantana2@senacsp.edu.br>
2022-04-28 20:38:48 -0300

    aprendendo sobre rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JogosComponent } from './views/jogos/jogos.component';
+import { PlataformasComponent } from './views/plataformas/plataformas.component';
+import { SobreComponent } from './views/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JogosComponent,
+    PlataformasComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/plataformas/plataformas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plataformas',
+  templateUrl: './plataformas.component.html',
+  styleUrls: ['./plataformas.component.css']
+})
+export class PlataformasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
