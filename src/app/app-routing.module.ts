732bd1fddf3db147636d21dc899312f5a85e71fa
VINICIUS LIMA DE SANTANA <vinicius.lsantana2@senacsp.edu.br>
2022-04-28 21:57:01 -0300

    material da aula

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JogosComponent } from './views/jogos/jogos.component';
+import { PlataformasComponent } from './views/plataformas/plataformas.component';
+import { SobreComponent } from './views/sobre/sobre.component';
 
 const routes: Routes = [
   {
     path: 'jogos',
     component: JogosComponent
+  },
+  {
+    path: 'plataformas',
+    component: PlataformasComponent
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
   }
 ];
 
